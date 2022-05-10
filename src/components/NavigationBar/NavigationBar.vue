30e45939810f686e5ee9fb3407330019639f7ce2
Definitely not xl_83 <LXALT69@outlook.com>
2022-05-10 04:07:28 +0000

    sassasasasasaas

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -60,7 +60,7 @@ const socket = io("https://s4d-test-server.herokuapp.com");
      function coords() {
          console.log("sending")
         const xmlContent = Blockly.Xml.domToPrettyText(Blockly.Xml.workspaceToDom(Blockly.Workspace));
-        socket.emit('coords', xmlContent);
+        socket.emit('sendCoords', xmlContent);
         requestAnimationFrame(coords)
     }
 
